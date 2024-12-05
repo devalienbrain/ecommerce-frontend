09a013d2dc9f0bfb27a4199d3574bc474b2605b5
sabbirhassanict <sabbirmurad.doict@gmail.com>
2024-12-05 16:47:12 +0600

    manage user page for admin role created: OK

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/Footer";
 import Dashboard from "./pages/dashboard/Dashboard";
 import { UserProvider } from "./provider/UserContext";
 import Profile from "./components/dashboards/Profile";
+import ManageUsers from "./components/dashboards/ManageUsers";
 
 function App() {
   return (
@@ -21,7 +22,9 @@ function App() {
 
           {/* Dashboard route with nested routes */}
           <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="" element={<Profile />} />
             <Route path="profile" element={<Profile />} />
+            <Route path="manage-users" element={<ManageUsers />} />
           </Route>
         </Routes>
         <Footer />
