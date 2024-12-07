1186762e7558d4a53ba3ef62f1d26394bba8b72c
sabbirhassanict <sabbirmurad.doict@gmail.com>
2024-12-08 01:03:13 +0600

    Get orders successfull: OK

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -14,21 +14,22 @@ const Orders = () => {
       .then((response) => setOrders(response?.data))
       .catch((error) => console.error("Error fetching orders:", error));
   }, [user?.id]);
-
+  console.log(orders);
   return (
     <div className="px-6 py-4 w-full lg:w-3/4 mx-auto my-6 min-h-screen">
       <h1 className="text-2xl md:text-4xl font-black text-center mb-5">
         My Orders
       </h1>
       <hr className="mb-7" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1  gap-4">
         {orders?.length > 0 ? (
           orders?.map((order) => (
             <div key={order?.id} className="border p-4 rounded shadow">
               <h3 className="font-bold text-lg">Order ID: {order?.id}</h3>
-              <p>Total: ${order?.total?.toFixed(2)}</p>
+              <p>Total: ${order?.totalPrice?.toFixed(2)}</p>
               <p>Status: {order?.status}</p>
               <p>Items: {order?.items?.length || 0}</p>
+              <p>Items: {order?.createdAt}</p>
             </div>
           ))
         ) : (
